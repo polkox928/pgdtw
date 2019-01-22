Fix scaling and normalization

final/libdtw.py
@@ -160,8 +160,8 @@ class Dtw:
             return
 
         distance_matrix = pairwise_distances(
-            X=reference_ts, Y=query_ts, metric=euclidean, n_jobs=n_jobs,
-            w=self.data['feat_weights'])
+            X=reference_ts, Y=query_ts, metric=euclidean, n_jobs=n_jobs, w=self.data['feat_weights']
+            )
 
         return distance_matrix
 
@@ -182,7 +182,7 @@ class Dtw:
                     i, j, acc_dist_matrix, distance_matrix, step_pattern)\
                     if self.itakura(i, j, ref_len, query_len, step_pattern) else np.inf
 
-        return acc_dist_matrix
+        return acc_dist_matrix#/(ref_len+query_len)
 
     def comp_acc_element(self, i, j, acc_dist_matrix, distance_matrix, step_pattern):
         """
@@ -503,6 +503,9 @@ class Dtw:
             return avgTD
 
     def AvgDistance(self, step_pattern):
+        """
+        Computes average 
+        """
         if len(self.data['distance_distortion'][step_pattern]) != self.data['num_queries']:
             print('Not every query aligned, align the remaining queries')
             return
@@ -513,16 +516,25 @@ class Dtw:
             return avgDist
 
     def GetPmax(self, query_id):
+        """
+        Computes the maximum value of P for the selected query batch
+        """
         Kq = len(self.data['queries'][query_id][0]['values'])
         Kr = len(self.data['reference'][0]['values'])
         p_max = np.floor(min(Kq, Kr)/abs(Kq - Kr)) if abs(Kq - Kr) > 0 else Kr
         return p_max
 
     def GetGlobalPmax(self):
+        """
+        Computes the maximum value of P for the data set under consideration
+        """
         p_maxs = [self.GetPmax(query_id) for query_id in self.data['queriesID']]
         return int(min(p_maxs))
 
     def itakura(self, i, j, ref_len, query_len, step_pattern):
+        """
+        Induced Itakura global constraint for GLOBAL ALIGNMENT
+        """
         patt = re.compile("symmetricP[1-9]+\d*")
         if step_pattern == "symmetricP05":
             p = 1/2
@@ -562,11 +574,17 @@ class Dtw:
         return (case, in_domain)
 
     def reset_weights(self):
+        """
+        Reset the variables' weights to 1 
+        """
         n_feat = len(self.data['reference'])
         weigths = np.ones(n_feat)
         self.data['feat_weights'] = weigths
 
     def compute_mld(self, distance_matrix, warping_path):
+        """
+        Compute the MLDs coefficients (mean local distance) of a certain distance_matrix relative to warping_path
+        """
         k = len(warping_path)
         on_path = [distance_matrix[i, j] for i, j in warping_path]
         on_path_mld = np.mean(on_path)
@@ -577,14 +595,19 @@ class Dtw:
                 'offpath': off_path_mld}
 
     def extract_single_feat(self, feat_idx, query_id):
-        reference_ts = np.array(self.data['reference'][feat_idx]['values']).reshape(-1, 1)
-        query_ts = np.array(self.data['queries'][query_id][feat_idx]['values']).reshape(-1, 1)
+        """
+        Accessory method for selecting single features from the dataset
+        """
+        reference_ts = np.array(self.scale_pv(" ", self.data['reference'][feat_idx]['values'])).reshape(-1, 1)
+        query_ts = np.array(self.scale_pv(" ", self.data['queries'][query_id][feat_idx]['values'])).reshape(-1, 1)
 
         return {'reference': reference_ts,
                 'query': query_ts}
 
     def weight_optimization_single_batch(self, query_id, step_pattern):
-
+        """
+        Optimization step regarding a single batch
+        """
         reference_ts = self.convert_to_mvts(self.data['reference'])
         query_ts = self.convert_to_mvts(self.data['queries'][query_id])
         res = self.dtw(reference_ts, query_ts, step_pattern=step_pattern)
@@ -609,6 +632,9 @@ class Dtw:
         return weights
 
     def weight_optimization_step(self, step_pattern='symmetric2', update=False):
+        """
+        Single iteration of the optimization algorithm, considering all batches in the instance
+        """
         tot_feats = len(self.data['reference'])
         num_queries = self.data['num_queries']
         w_matrix = np.empty((num_queries, tot_feats))
@@ -626,6 +652,9 @@ class Dtw:
         return updated_weights
 
     def optimize_weigths(self, step_pattern='symmetric2', convergence_threshold=0.01, n_steps=10):
+        """
+        Implements the algorithm for the optimization of the weights
+        """
         current_weights = self.data['feat_weights']
         conv_val = 1
         step = 0
@@ -642,11 +671,17 @@ class Dtw:
         self.data['feat_weights'] = updated_weights
 
     def get_weight_variables(self):
+        """
+        Returns a dictionary with the weight for each variable
+        """
         vars = [pv['name'] for pv in self.data['reference']]
         var_weight = {var: weight for var, weight in zip(vars, self.data['feat_weights'])}
         return var_weight
 
     def plot_weights(self, n=25, figsize=(15, 8)):
+        """
+        Horizontal bar chart with variables' weights sorted by magnitude
+        """
         plt.rcdefaults()
         fig, ax = plt.subplots(figsize=figsize)
 
@@ -665,6 +700,22 @@ class Dtw:
 
         fig.tight_layout()
         plt.show()
+        
+    def plot_by_name(self, _id, pv_name):
+        """
+        Plots one pv relative to a batch with ID equal to _id, according to its name pv_name
+        """
+        pv_list = [pv['name'] for pv in self.data['reference']]
+        
+        if _id != self.data['ref_id']:
+            pv_idx = pv_list.index(pv_name)
+            plt.plot(self.data['queries'][_id][pv_idx]['values'])
+            plt.show()
+        elif _id == self.data['ref_id']:
+            pv_idx = pv_list.index(pv_name)
+            plt.plot(self.data['reference'][pv_idx]['values'])
+            plt.show()
+        else: print('Batch ID not found')
 
 
 def load_data(n_to_keep=50):

final/local_constraint_selection.py
@@ -14,7 +14,7 @@ import libdtw as lib
 try:
     N_DATA = int(sys.argv[1])
 except LookupError as ex:
-    N_DATA = 50
+    N_DATA = 25
 
 try:
     with open('dtwObj%d.pickle' % N_DATA, 'rb') as f:
@@ -22,9 +22,12 @@ try:
 except OSError as ex:
     DATA = lib.load_data(N_DATA)
     D = lib.Dtw(DATA)
-
+    with open('dtwObjOptWeights5AllFeats.pickle', 'rb') as f:
+        D_weights = pickle.load(f)
+    
+D.data['feat_weights'] = D_weights
 POSSIBLE_STEP_PATTERNS = ['symmetricP05']
-POSSIBLE_P_PATTERNS = ['symmetricP%s' % p for p in np.arange(1, D.GetGlobalPmax()+1)]
+POSSIBLE_P_PATTERNS = ['symmetricP%s'%p for p in np.arange(1, D.GetGlobalPmax()+1)]
 
 POSSIBLE_STEP_PATTERNS.extend(POSSIBLE_P_PATTERNS)
 # %%
@@ -44,38 +47,40 @@ with open('dtwObj%d.pickle' % N_DATA, 'wb') as f:
 
 # %%
 #POSSIBLE_STEP_PATTERNS = [POSSIBLE_STEP_PATTERNS[0]] + POSSIBLE_STEP_PATTERNS[2:]
-# TD = [x[0] for x in RES.values()]
-# RANGE_TD = min(TD), max(TD)
-#
-# DIST = [x[1] for x in RES.values()]
-# RANGE_DIST = min(DIST), max(DIST)
-#
-# RES_SCALED = defaultdict(list)
-# for step_pattern in POSSIBLE_STEP_PATTERNS:
-#     RES_SCALED[step_pattern] = [(RES[step_pattern][0] - RANGE_TD[0])/(RANGE_TD[1]-RANGE_TD[0])]
-#     RES_SCALED[step_pattern].append(
-#         (RES[step_pattern][1] - RANGE_DIST[0])/(RANGE_DIST[1]-RANGE_DIST[0]))
-#
-#
-# DISTANCES = [RES_SCALED[step_pattern][0] for step_pattern in POSSIBLE_STEP_PATTERNS]
-# DISTORTIONS = [RES_SCALED[step_pattern][1] for step_pattern in POSSIBLE_STEP_PATTERNS]
-# SCORE = [np.sqrt(x**2 + y**2) for x, y in zip(DISTANCES, DISTORTIONS)]
+TD = [x[0] for x in RES.values()]
+RANGE_TD = min(TD), max(TD)
 
-# X = np.arange(1, len(SCORE)+1)
-# FIG = plt.figure(figsize=(12, 5))
-# FIG.add_subplot(1, 2, 1)
-# plt.plot(X, SCORE, '-o')
-# plt.xticks(X, POSSIBLE_STEP_PATTERNS, rotation="vertical")
-# plt.ylabel("Alignment SCORE")
-#
-# FIG.add_subplot(1, 2, 2)
-# plt.plot(DISTANCES, DISTORTIONS, '-o')
-# plt.xlabel("Scaled distance")
-# plt.ylabel("Scaled distortion")
-# plt.annotate('P05', xy=(DISTANCES[0], DISTORTIONS[0]), xytext=(-4, 5), textcoords='offset pixels')
-# for x, y, label in zip(DISTANCES[1:6], DISTORTIONS[1:6], POSSIBLE_STEP_PATTERNS[1:6]):
-#     plt.annotate(label[label.index('P'):], xy=(x, y), xytext=(5, 5), textcoords='offset pixels')
-#
-# for x, y, label in zip(DISTANCES[8::3], DISTORTIONS[8::3], POSSIBLE_STEP_PATTERNS[8::3]):
-#     plt.annotate(label[label.index('P'):], xy=(x, y), xytext=(5, 0), textcoords='offset pixels')
-# plt.show()
+DIST = [x[1] for x in RES.values()]
+RANGE_DIST = min(DIST), max(DIST)
+
+RES_SCALED = defaultdict(list)
+for step_pattern in POSSIBLE_STEP_PATTERNS:
+    RES_SCALED[step_pattern] = [(RES[step_pattern][0] - RANGE_TD[0])/(RANGE_TD[1]-RANGE_TD[0])]
+    RES_SCALED[step_pattern].append((RES[step_pattern][1] - RANGE_DIST[0])/(RANGE_DIST[1]-RANGE_DIST[0]))
+
+
+DISTANCES = [RES_SCALED[step_pattern][0] for step_pattern in POSSIBLE_STEP_PATTERNS]
+DISTORTIONS = [RES_SCALED[step_pattern][1] for step_pattern in POSSIBLE_STEP_PATTERNS]
+SCORE = [np.sqrt(x**2 + y**2) for x, y in zip(DISTANCES, DISTORTIONS)]
+
+X = np.arange(1, len(SCORE)+1)
+#%%
+FIG = plt.figure(figsize=(12, 5))
+FIG.add_subplot(1, 2, 1)
+
+plt.plot(X, SCORE, '-o', color = '#d90000')
+plt.xticks(X, POSSIBLE_STEP_PATTERNS, rotation="vertical")
+plt.ylabel("Alignment SCORE")
+
+FIG.add_subplot(1, 2, 2)
+plt.plot(DISTANCES, DISTORTIONS, '-o', color = '#d90000')
+plt.xlabel("Scaled distance")
+plt.ylabel("Scaled distortion")
+plt.annotate('P05', xy=(DISTANCES[0], DISTORTIONS[0]), xytext=(-4, 5), textcoords='offset pixels')
+for x, y, label in zip(DISTANCES[1:6], DISTORTIONS[1:6], POSSIBLE_STEP_PATTERNS[1:6]):
+    plt.annotate(label[label.index('P'):], xy=(x, y), xytext=(5, 5), textcoords='offset pixels')
+
+for x, y, label in zip(DISTANCES[8::3], DISTORTIONS[8::3], POSSIBLE_STEP_PATTERNS[8::3]):
+    plt.annotate(label[label.index('P'):], xy=(x, y), xytext=(5, 0), textcoords='offset pixels')
+plt.tight_layout()
+plt.show()

final/test.py
@@ -49,8 +49,8 @@ for _id, pvs in data.items():
     for pv in pvs:
         pvList.append(pv['name'])
     pvDataset.append(pvList)
-plt.hist([l for l, _id in opeLen], bins=50)
-plt.show()
+#plt.hist([l for l, _id in opeLen], bins=50)
+#plt.show()
 
 medLen = np.median([l for l, _id in opeLen])
 
@@ -58,8 +58,8 @@ medLen = np.median([l for l, _id in opeLen])
 # center around the median
 centered = [(abs(l-medLen), _id) for l, _id in opeLen]
 selected = sorted(centered)[:50]
-plt.hist([l for l, _id in opeLen if _id in [ID for d, ID in selected]], bins = 20)
-plt.show()
+#plt.hist([l for l, _id in opeLen if _id in [ID for d, ID in selected]], bins = 20)
+#plt.show()
 med_id = selected[0][1] #5153
 print(med_id)
 # pop batches without all pvs
@@ -85,7 +85,7 @@ d = lib.Dtw(data)
 ref = d.convert_to_mvts(d.data['reference'])
 query = d.convert_to_mvts(d.data['queries']['5300'])
 
-step_pattern = "symmetricP05"
+step_pattern = "symmetric2"
 res = d.dtw(ref, query, open_ended=False, step_pattern=step_pattern, n_jobs = 1)
 warp = res['warping']
 
@@ -93,6 +93,7 @@ print(res['DTW_distance'])
 print(res['warping'][-1])
 
 fig=plt.figure(figsize=(12, 5))
+fig.suptitle(step_pattern)
 fig.add_subplot(1,2,1)
 d.distance_cost_plot(d.comp_dist_matrix(ref, query))
 plt.plot([x[1] for x in warp], [x[0] for x in warp])

final/weight_optimization.py
@@ -4,20 +4,17 @@ Created on Fri Jan 18 14:10:58 2019
 
 @author: DEPAGRA
 """
-
 import libdtw as lib
 import sys
 import numpy as np
 import pickle
-from pprint import pprint
-import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=2)
 
 try:
     N_DATA = int(sys.argv[1])
 except LookupError as ex:
-    N_DATA = 5
+    N_DATA = 3
 
 DATA = lib.load_data(N_DATA)
 D = lib.Dtw(DATA)